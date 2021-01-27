42434c0882726d87d6dddf7679fbce1592dd9e75
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-01-27 07:09:45 +0100

    add the google Authentication

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
-  styleUrls: ['./bs-navbar.component.css']
+  styleUrls: ['./bs-navbar.component.css'],
 })
-export class BsNavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BsNavbarComponent {
+  
+  user: firebase.User;
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user);
   }
 
+  logout() {
+    this.afAuth.signOut();
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class LoginComponent {
+  constructor(private afAuth: AngularFireAuth) {
+   
   }
 
+  login() {
+    this.afAuth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
 }
