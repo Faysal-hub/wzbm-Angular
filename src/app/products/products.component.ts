2247226325dd7487bf53ec2862df18127501daf5
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-02-17 02:22:05 +0100

    complete product catalog Module

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,6 @@
 import { ActivatedRoute } from '@angular/router';
-import { CategoryService } from './../category.service';
 import { Product } from './../models/product';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -13,13 +12,12 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$;
+  
   category: string;
 
   constructor(
     route: ActivatedRoute,
-    productService: ProductService,
-    categoryService: CategoryService
+    productService: ProductService,     
   ) {
     productService
       .getAll()
@@ -33,15 +31,19 @@ export class ProductsComponent {
           })
         )
       )
-      .subscribe(products => this.products = products);
-    this.categories$ = categoryService.getAll();
-
-    route.queryParamMap.subscribe((params) => {
+      .pipe(switchMap(products =>{
+        this.products = products; 
+        return route.queryParamMap;
+      }))
+      .subscribe((params) => {
       this.category = params.get('category');
 
       this.filteredProducts = this.category
         ? this.products.filter((p) => p.category === this.category)
         : this.products;
     });
+
+    
+
   }
 }
